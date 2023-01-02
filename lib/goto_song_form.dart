18fa94213f15821460ccfe403fa859cadb4cdc64
Fodor Benedek <benedek@fodor.pro>
2023-01-02 14:32:08 +0100

    goto_song_form: Ugrás gomb billentyűzet fölé helyezése closes: #6

--- lib/goto_song_form.dart
+++ lib/goto_song_form.dart
@@ -24,78 +24,99 @@ class MyGotoSongForm extends StatefulWidget {
 // @see https://docs.flutter.dev/cookbook/forms/validation
 class _MyGotoSongFormState extends State<MyGotoSongForm> {
   final _formKey = GlobalKey<FormState>();
-
+  late TextEditingController controller;
   // @see https://stackoverflow.com/q/63492002
   final _myFocusNode = FocusNode();
 
   @override
   Widget build(BuildContext context) {
-    var controller = TextEditingController();
+    controller = TextEditingController();
     return Scaffold(
       appBar: AppBar(
         title: const Text('Ugrás énekre'),
       ),
-      body: Form(
-        key: _formKey,
-        child: Container(
-          padding: const EdgeInsets.all(30.0),
-          child: Column(
-            children: [
-              PlatformAwareTextFormField(
-                labelText: 'Ének száma:',
-                helperText: '(1 és ${widget.songs.keys.last} között)',
-                focusNode: _myFocusNode,
-                controller: controller,
-                onFieldSubmitted: (details) {
-                  // Validate returns true if the form is valid, or false
-                  // otherwise.
-                  if (_formKey.currentState!.validate()) {
-                    // If the form is valid, display the given song page.
-                    Navigator.of(context).push(
-                      MaterialPageRoute(
-                        builder: (context) {
-                          return MySongPage(
-                            songsInBook: widget.songs,
-                            bookProvider: widget.bookProvider,
-                            // As we want to be able to turn page by page (ie.
-                            // verse by verse), we need to go to the Nth song
-                            // in the book (from the ListView the user sees).
-                            // So we need to look up the index of the song
-                            // whose number was entered (eg. for the 2021
-                            // book, we need the 198th song from the list when
-                            // the user wants to navigate to song #201).
-                            songIndex:
-                                widget.songs.keys.toList().indexOf(details),
-                          );
-                        },
-                      ),
-                    );
-                    // Clear the TextFormField after returning from the song
-                    // page.
-                    // @see https://stackoverflow.com/a/57747739
-                    controller.clear();
-                  }
-                  // Keep the TextFormField focused for both of these cases:
-                  // - If the form is invalid.
-                  // - When we're returning from the song page.
-                  _myFocusNode.requestFocus();
-                },
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Írj be egy számot!';
-                  }
-                  if (!widget.songs.containsKey(value)) {
-                    return 'Nincs ilyen ének.';
-                  }
-                  return null;
-                },
-                key: const Key('_MyCustomFormState.TextFormField'),
+      body: Column(
+        children: [
+          Expanded(
+            child: Form(
+              key: _formKey,
+              child: ListView(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(10, 20, 10, 0),
+                    child: PlatformAwareTextFormField(
+                      labelText: 'Ének száma:',
+                      helperText: '(1 és ${widget.songs.keys.last} között)',
+                      focusNode: _myFocusNode,
+                      controller: controller,
+                      onFieldSubmitted: onFieldSubmitted,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Írj be egy számot!';
+                        }
+                        if (!widget.songs.containsKey(value)) {
+                          return 'Nincs ilyen ének.';
+                        }
+                        return null;
+                      },
+                      key: const Key('_MyCustomFormState.TextFormField'),
+                    ),
+                  ),
+                ],
               ),
-            ],
+            ),
           ),
-        ),
+          if (Platform.isIOS) //Platform.isIOS)
+            Material(
+              color: Theme.of(context).bottomAppBarColor,
+              child: Row(mainAxisAlignment: MainAxisAlignment.end, children: [
+                // @ todo future place of 'Tovább' button for entering verse number
+                SizedBox(
+                    width: 100,
+                    child: CupertinoDialogAction(
+                        child: const Text('Ugrás'),
+                        onPressed: () {
+                          onFieldSubmitted(controller.text);
+                        })),
+              ]),
+            )
+        ],
       ),
     );
+  }
+
+  onFieldSubmitted(details) {
+    // Validate returns true if the form is valid, or false
+    // otherwise.
+    if (_formKey.currentState!.validate()) {
+      // If the form is valid, display the given song page.
+      Navigator.of(context).push(
+        MaterialPageRoute(
+          builder: (context) {
+            return MySongPage(
+              songsInBook: widget.songs,
+              bookProvider: widget.bookProvider,
+              // As we want to be able to turn page by page (ie.
+              // verse by verse), we need to go to the Nth song
+              // in the book (from the ListView the user sees).
+              // So we need to look up the index of the song
+              // whose number was entered (eg. for the 2021
+              // book, we need the 198th song from the list when
+              // the user wants to navigate to song #201).
+              songIndex: widget.songs.keys.toList().indexOf(details),
+            );
+          },
+        ),
+      );
+      // Clear the TextFormField after returning from the song
+      // page.
+      // @see https://stackoverflow.com/a/57747739
+      controller.clear();
+    }
+    // Keep the TextFormField focused for both of these cases:
+    // - If the form is invalid.
+    // - When we're returning from the song page.
+    _myFocusNode.requestFocus();
   }
 }
 
@@ -119,55 +140,38 @@ class PlatformAwareTextFormField extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Platform.isAndroid
-        ? TextFormField(
-            decoration: InputDecoration(
-              labelText: labelText,
-              helperText: helperText,
-            ),
-            focusNode: focusNode,
-            controller: controller,
-            onFieldSubmitted: onFieldSubmitted,
-            validator: validator,
-            autofocus: true,
-            // @see https://stackoverflow.com/q/49577781
-            keyboardType: TextInputType.number,
-            inputFormatters: [
-              FilteringTextInputFormatter.digitsOnly,
-            ],
-          )
-        : CupertinoTheme(
+    return Platform.isIOS
+        ? CupertinoTheme(
             data: const CupertinoThemeData(brightness: Brightness.dark),
-            child: Column(
-              children: [
-                CupertinoTextFormFieldRow(
-                  prefix: Text(labelText),
-                  placeholder: helperText,
-                  autofocus: true,
-                  controller: controller,
-                  // @see https://stackoverflow.com/q/49577781
-                  keyboardType: TextInputType.number,
-                  inputFormatters: [
-                    FilteringTextInputFormatter.digitsOnly,
-                  ],
-                  validator: validator,
-                  onFieldSubmitted: onFieldSubmitted,
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 60),
-                  child: CupertinoButton(
-                    // Need a separate button on iOS; number keyboard does not
-                    // have a Done button.
-                    onPressed: () {
-                      onFieldSubmitted(controller.text);
-                    },
-                    color: CupertinoTheme.of(context).barBackgroundColor,
-                    child: Icon(
-                      Icons.check,
-                      color: CupertinoTheme.of(context).primaryColor,
-                    ),
-                  ),
-                )
+            child: CupertinoTextFormFieldRow(
+              prefix: Text(labelText),
+              placeholder: helperText,
+              autofocus: true,
+              controller: controller,
+              // @see https://stackoverflow.com/q/49577781
+              keyboardType: TextInputType.number,
+              inputFormatters: [
+                FilteringTextInputFormatter.digitsOnly,
+              ],
+              validator: validator,
+              onFieldSubmitted: onFieldSubmitted,
+            ))
+        : Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            child: TextFormField(
+              decoration: InputDecoration(
+                labelText: labelText,
+                helperText: helperText,
+              ),
+              focusNode: focusNode,
+              controller: controller,
+              onFieldSubmitted: onFieldSubmitted,
+              validator: validator,
+              autofocus: true,
+              // @see https://stackoverflow.com/q/49577781
+              keyboardType: TextInputType.number,
+              inputFormatters: [
+                FilteringTextInputFormatter.digitsOnly,
               ],
             ),
           );
