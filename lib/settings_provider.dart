bc14776842157f9cfe85a7de0f599e6e01c7521f
Fodor Benedek <benedek@fodor.pro>
2023-01-06 18:18:26 +0100

    Új beállítás mezők hozzáadása providerhez

--- lib/settings_provider.dart
+++ lib/settings_provider.dart
@@ -5,23 +5,23 @@ class SettingsProvider extends ChangeNotifier {
   static const Book defaultBook = Book.blue;
   static const ScoreDisplay defaultScoreDisplay = ScoreDisplay.all;
   static const double defaultFontSize = 14.0;
+  static const BrightnessSetting defaultAppBrightnessSetting =
+      BrightnessSetting.system;
+  static const BrightnessSetting defaultSheetBrightnessSetting =
+      BrightnessSetting.light;
 
   Book _book = defaultBook;
   ScoreDisplay _scoreDisplay = defaultScoreDisplay;
   double _fontSize = defaultFontSize;
+  BrightnessSetting _appBrightnessSetting = defaultAppBrightnessSetting;
+  BrightnessSetting _sheetBrightnessSetting = defaultSheetBrightnessSetting;
   bool _initialized = false;
 
-  Book get book {
-    return _book;
-  }
-
-  ScoreDisplay get scoreDisplay {
-    return _scoreDisplay;
-  }
-
-  double get fontSize {
-    return _fontSize;
-  }
+  Book get book => _book;
+  ScoreDisplay get scoreDisplay => _scoreDisplay;
+  double get fontSize => _fontSize;
+  BrightnessSetting get appBrightnessSetting => _appBrightnessSetting;
+  BrightnessSetting get sheetBrightnessSetting => _sheetBrightnessSetting;
 
   String get bookAsString {
     switch (_book) {
@@ -34,9 +34,7 @@ class SettingsProvider extends ChangeNotifier {
     }
   }
 
-  bool get initialized {
-    return _initialized;
-  }
+  bool get initialized => _initialized;
 
   void changeBook(Book value) async {
     _book = value;
@@ -65,9 +63,26 @@ class SettingsProvider extends ChangeNotifier {
     _initialized = true;
   }
 
+  void changeAppBrightnessSetting(BrightnessSetting value) async {
+    _appBrightnessSetting = value;
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setInt('appBrightness', value.index);
+    notifyListeners();
+    //? I don't think seting _initialized to true is appropriate here.
+    //? Other values have not been loaded yet. (-RedyAu)
+  }
+
+  void changeSheetBrightnessSetting(BrightnessSetting value) async {
+    _appBrightnessSetting = value;
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setInt('sheetBrightness', value.index);
+    notifyListeners();
+  }
+
   void initialize() async {
     final SharedPreferences prefs = await SharedPreferences.getInstance();
 
+    //! Book
     try {
       // First try migrating from previous version.
       String? bookMigrateString = prefs.getString('book');
@@ -80,14 +95,28 @@ class SettingsProvider extends ChangeNotifier {
         // Read saved value.
         _book = Book.values[prefs.getInt('bookEnum') ?? defaultBook.index];
       }
+
+      //! Score apperance
+      _scoreDisplay = ScoreDisplay.values[
+          prefs.getInt('scoreDisplayEnum') ?? defaultScoreDisplay.index];
+
+      _fontSize = prefs.getDouble('fontSize') ?? defaultFontSize;
+
+      //! Brightness
+      _appBrightnessSetting = BrightnessSetting.values[
+          prefs.getInt('appBrightness') ?? defaultAppBrightnessSetting.index];
+
+      _sheetBrightnessSetting = BrightnessSetting.values[
+          prefs.getInt('sheetBrightness') ??
+              defaultSheetBrightnessSetting.index];
     } catch (e) {
+      // On any unexpected error, use default settings
       _book = defaultBook;
+      _scoreDisplay = defaultScoreDisplay;
+      _fontSize = defaultFontSize;
+      _appBrightnessSetting = defaultAppBrightnessSetting;
+      _sheetBrightnessSetting = defaultSheetBrightnessSetting;
     }
-
-    _scoreDisplay = ScoreDisplay
-        .values[prefs.getInt('scoreDisplayEnum') ?? defaultScoreDisplay.index];
-
-    _fontSize = prefs.getDouble('fontSize') ?? defaultFontSize;
 
     notifyListeners();
     _initialized = true;
@@ -120,5 +149,19 @@ String getScoreDisplayName(ScoreDisplay scoreDisplay) {
     case ScoreDisplay.all:
     default:
       return 'Minden vers';
+  }
+}
+
+enum BrightnessSetting { system, dark, light }
+
+String getBrightnessName(BrightnessSetting brightness) {
+  switch (brightness) {
+    case BrightnessSetting.system:
+      return "Rendszer";
+    case BrightnessSetting.dark:
+      return "Sötét";
+    case BrightnessSetting.light:
+    default:
+      return "Világos";
   }
 }
