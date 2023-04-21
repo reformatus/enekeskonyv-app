4fb1707bd947b2f2f5a7bf86429ec38f72c003ed
Fodor Benedek <benedek@fodor.pro>
2023-04-21 15:49:36 +0200

    init VerseBar

---
+++ lib/song/verse_bar.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class VerseBar extends StatelessWidget {
+  const VerseBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox(
+      height: 20,
+      child: Text('Hello VerseBar!'),
+    );
+  }
+}
