2b3f732ecc75401b56a06b9a37ff486be80cf645
Fodor Benedek <benedek@fodor.pro>
2023-10-13 23:38:20 +0200

    verseBar: switch to own scrollable widget. add faded edges. add scroll indicator chevrons.

--- lib/song/verse_bar.dart
+++ lib/song/verse_bar.dart
@@ -1,81 +1,122 @@
 import 'package:enekeskonyv/settings_provider.dart';
 import 'package:enekeskonyv/song/song_page_state.dart';
+import 'package:fading_edge_scrollview/fading_edge_scrollview.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-class VerseBar extends StatelessWidget {
+class VerseBar extends StatefulWidget {
   const VerseBar({super.key});
+
+  @override
+  State<VerseBar> createState() => _VerseBarState();
+}
+
+class _VerseBarState extends State<VerseBar> {
+  bool startEllipsis = false;
+  bool endEllipsis = false;
+  late ScrollController scrollController;
+
+  updateEndEllipsies(position) {
+    if (position.atEdge) {
+      setState(() {
+        startEllipsis = position.pixels == 0 ? false : true;
+        endEllipsis =
+            !startEllipsis && position.maxScrollExtent > position.pixels;
+      });
+    } else {
+      setState(() {
+        startEllipsis = true;
+        endEllipsis = true;
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    scrollController = ScrollController();
+    scrollController.addListener(() {
+      updateEndEllipsies(scrollController.position);
+    });
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      updateEndEllipsies(scrollController.position);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Consumer2<SettingsProvider, SongStateProvider>(
         builder: (context, settings, state, child) {
-      return (settings.scoreDisplay == ScoreDisplay.all)
-          ? Listener(
-              // Making sure the verse bar is shown when the user
-              // interacts with it.
-              onPointerHover: (_) => state.showThenHideVerseBar(),
-              onPointerMove: (_) => state.showThenHideVerseBar(),
-              child: SizedBox(
-                height: 50,
-                child: Row(
-                  children: [
-                    // Empty box to make the tab bar centered
-                    const SizedBox(width: 50),
-                    Expanded(
-                      child: Center(
+      if ((settings.scoreDisplay == ScoreDisplay.all)) {
+        return Listener(
+          // Making sure the verse bar is shown when the user
+          // interacts with it.
+          onPointerHover: (_) => state.showThenHideVerseBar(),
+          onPointerMove: (_) => state.showThenHideVerseBar(),
+          child: SizedBox(
+            height: 50,
+            child: Row(
+              children: [
+                // Empty box to make the tab bar centered
+                const SizedBox(width: 40),
+                AnimatedOpacity(
+                    duration: const Duration(milliseconds: 200),
+                    opacity: startEllipsis ? 1 : 0,
+                    child: Icon(Icons.chevron_left,
+                        color: Theme.of(context).disabledColor)),
+                Expanded(
+                  child: Center(
+                    child: FadingEdgeScrollView.fromSingleChildScrollView(
+                      shouldDisposeScrollController: true,
+                      child: SingleChildScrollView(
+                        controller: scrollController,
+                        scrollDirection: Axis.horizontal,
                         child: Card(
                           elevation: 3,
-                          // The tab bar is animated so that when the number
-                          // of verses changes, the tab bar will be resized
-                          // to fit the new number of verses with an animation.
-                          child: AnimatedSize(
-                            duration: const Duration(milliseconds: 300),
-                            curve: Curves.easeInOutCubic,
-                            child: TabBar(
-                              indicator: BoxDecoration(
-                                borderRadius: BorderRadius.circular(10),
-                                color: Theme.of(context)
-                                    .colorScheme
-                                    .primaryContainer,
-                              ),
-                              indicatorSize: TabBarIndicatorSize.tab,
-                              dividerColor: Colors.transparent,
-                              isScrollable: true,
-                              labelPadding:
-                                  const EdgeInsets.symmetric(horizontal: 20),
-                              automaticIndicatorColorAdjustment: false,
-                              controller: state.tabController,
-                              tabs: [
-                                for (var i = 0;
-                                    i < state.tabController.length;
-                                    i++)
-                                  // Get the verse number from the text itself.
-                                  // The 48 book skips some verses.
-                                  Tab(
-                                    text: songBooks[state.book.name]
-                                            [state.songKey]['texts'][i]
-                                        .split('.')[0],
-                                  ),
-                              ],
+                          child: TabBar(
+                            indicator: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              color: Theme.of(context)
+                                  .colorScheme
+                                  .primaryContainer,
                             ),
+                            indicatorSize: TabBarIndicatorSize.tab,
+                            dividerColor: Colors.transparent,
+                            labelPadding:
+                                const EdgeInsets.symmetric(horizontal: 20),
+                            automaticIndicatorColorAdjustment: false,
+                            controller: state.tabController,
+                            isScrollable: true,
+                            tabs: state.tabs,
                           ),
                         ),
                       ),
                     ),
-                    // Pin button
-                    IconButton(
-                        onPressed: () => settings
-                            .changeIsVerseBarPinned(!settings.isVerseBarPinned),
-                        icon: const Icon(Icons.push_pin),
-                        color: settings.isVerseBarPinned
-                            ? Theme.of(context).colorScheme.secondary
-                            : Theme.of(context).disabledColor)
-                  ],
+                  ),
                 ),
-              ),
-            )
-          : Container();
+                AnimatedOpacity(
+                    duration: const Duration(milliseconds: 200),
+                    opacity: endEllipsis ? 1 : 0,
+                    child: Icon(Icons.chevron_right,
+                        color: Theme.of(context).disabledColor)),
+                // Pin button
+                SizedBox(
+                  width: 40,
+                  child: IconButton(
+                      onPressed: () => settings
+                          .changeIsVerseBarPinned(!settings.isVerseBarPinned),
+                      icon: const Icon(Icons.push_pin),
+                      color: settings.isVerseBarPinned
+                          ? Theme.of(context).colorScheme.secondary
+                          : Theme.of(context).disabledColor),
+                )
+              ],
+            ),
+          ),
+        );
+      } else {
+        return Container();
+      }
     });
   }
 }
