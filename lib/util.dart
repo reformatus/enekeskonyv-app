2759d6d42fdea3c23e20d2a59e1735fdd8af706e
Fodor Benedek <benedek@fodor.pro>
2023-10-17 19:22:57 +0200

    add getVerseId util

--- lib/util.dart
+++ lib/util.dart
@@ -1,5 +1,11 @@
 import 'dart:collection';
 
+import 'settings_provider.dart';
+
 String getSongTitle(LinkedHashMap song) {
   return (song['number'] != null ? song['number'] + ': ' : '') + song['title'];
+}
+
+String getVerseId(Book book, String songKey, int verse) {
+  return '${book.name}/$songKey/$verse';
 }
