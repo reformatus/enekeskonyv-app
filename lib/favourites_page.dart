c61b98ce12d8088d9f4531ef063dc897a7680353
Fodor Benedek <benedek@fodor.pro>
2023-10-20 00:40:51 +0200

    favourites page is now cuelist

--- lib/favourites_page.dart
+++ lib/favourites_page.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:enekeskonyv/settings_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -34,18 +36,15 @@ class FavouritesPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    Map<String, Map<String, Set<String>>> favourites =
-        getFavourites(SettingsProvider.of(context).getSelectedCueContent());
-
     return Consumer<SettingsProvider>(
       builder: (context, settings, child) {
         return Scaffold(
-          appBar: AppBar(
-            title: const Text('Kedvencek'),
-            actions: [
-              PopupMenuButton(
-                itemBuilder: (i) => [
-                  /*// Import button
+            appBar: AppBar(
+              title: const Text('Kedvencek'),
+              actions: [
+                PopupMenuButton(
+                  itemBuilder: (i) => [
+                    /*// Import button
                   const PopupMenuItem(
                     enabled: false,
                     child: ListTile(
@@ -61,139 +60,148 @@ class FavouritesPage extends StatelessWidget {
                       title: Text('Exportálás'),
                     ),
                   ),*/
-                  // Delete all button
-                  PopupMenuItem(
-                    onTap: () => settings.clearCue(settings.selectedCue),
-                    child: const ListTile(
-                      leading: Icon(
-                        Icons.delete_forever,
-                        color: Colors.red,
+                    // Delete all button
+                    PopupMenuItem(
+                      onTap: () => settings.clearCue(settings.selectedCue),
+                      child: const ListTile(
+                        leading: Icon(
+                          Icons.delete_forever,
+                          color: Colors.red,
+                        ),
+                        title: Text('Összes törlése'),
                       ),
-                      title: Text('Összes törlése'),
                     ),
-                  ),
-                ],
-              )
-            ],
-          ),
-          body: ListView(
-            children: [
-              bookTile(favourites, Book.black, settings),
-              bookTile(favourites, Book.blue, settings),
-              ListTile(
-                subtitle: Text(
-                  '''
-Megjelölhetsz versszakokat kedvencként.
-
-A jelöléshez használd a versválasztó sáv melletti csillag gombot, ha minden kottát megjelenítesz az appban.
-
-Ha nem jelenítesz meg minden kottát, az adott versszakot tartsd hosszan lenyomva a hozzáadáshoz.''',
-                  style: TextStyle(
-                      fontStyle: FontStyle.italic,
-                      color: Theme.of(context).colorScheme.secondary),
-                ),
-              )
-            ],
-          ),
-        );
+                  ],
+                )
+              ],
+            ),
+            body: ReorderableListView(
+              onReorder: (oldIndex, newIndex) =>
+                  settings.reorderCue(settings.selectedCue, oldIndex, newIndex),
+              physics: Platform.isIOS ? const BouncingScrollPhysics() : null,
+              children: getVerseTiles(settings),
+            ));
       },
     );
   }
 
-  Widget bookTile(Map<String, Map<String, Set<String>>> favourites, Book book,
-      SettingsProvider settings) {
-    if (favourites.keys.contains(book.name) &&
-        favourites[book.name]!.isNotEmpty) {
-      var songs = favourites[book.name]!.keys.toList();
-      songs.sort();
+  List<Widget> getVerseTiles(SettingsProvider settings) {
+    List<Widget> verseTiles = [];
 
-      return Theme(
-        data: Theme.of(context).copyWith(dividerColor: Colors.transparent),
-        child: ExpansionTile(
-          initiallyExpanded: settings.book == book,
-          title: Text(book.displayName, style: const TextStyle(fontSize: 19)),
-          children: songs.map((songKey) {
-            var verses = favourites[book.name]![songKey]!.toList();
-            verses.sort();
+    int i = 0;
+    String lastBook = "";
+    String lastSong = "";
+    for (String verseId in settings.getSelectedCueContent()) {
+      List<String> parts = verseId.split('/');
+      String bookName = parts[0];
+      String songKey = parts[1];
+      int verseIndex = int.parse(parts[2]);
 
-            return songCard(
-              book,
-              songKey,
-              verses
-                  .map((verseIndex) =>
-                      verseTile(book, songKey, int.parse(verseIndex), 1, settings)) // TOOD replace 1 with actual cue index
-                  .toList(),
-            );
-          }).toList(),
-        ),
+      verseTiles.add(
+        // Had to factor out for tile removing closure to work properly (???)
+        verseTile(bookName, songKey, verseIndex, lastBook != bookName,
+            lastSong != songKey, i, settings),
       );
-    } else {
-      return const SizedBox();
+      lastBook = bookName;
+      lastSong = songKey;
+
+      i++;
     }
+
+    return verseTiles;
   }
 
-  Widget verseTile(
-      Book book, String songKey, int verseIndex, int cueIndex, SettingsProvider settings) {
+  Widget verseTile(String bookName, String songKey, int verseIndex,
+      bool newBook, bool newSong, int cueIndex, SettingsProvider settings) {
+    Book book = Book.values.firstWhere((b) => b.name == bookName);
+    String verse = songBooks[bookName][songKey]['texts'][verseIndex];
+    String verseNumber = verse.split('.')[0];
+    String verseText = verse.substring(verseNumber.length + 2);
+
     return InkWell(
-      onTap: () => Navigator.of(context).push(
-        MaterialPageRoute(
-          builder: (context) {
-            return SongPage(
-              book: book,
-              songIndex: songBooks[book.name].keys.toList().indexOf(songKey),
-              // HACK Could be better handled on song page.
-              // Maybe even implement scrolling to matching verse there
-              // when showing texts?
-              verseIndex: (SettingsProvider.of(context).scoreDisplay ==
-                      ScoreDisplay.all)
-                  ? verseIndex
-                  : 0,
-            );
-          },
-        ),
-      ),
-      child: Container(
-        decoration: BoxDecoration(
-          border: Border(
-            left: BorderSide(
-              color: Theme.of(context).colorScheme.outline,
-              width: 1,
+      key: GlobalKey(),
+      onTap: () {
+        Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (context) {
+              return SongPage(
+                book: book,
+                songIndex: songBooks[book.name].keys.toList().indexOf(songKey),
+                verseIndex: verseIndex,
+                initialCueIndex: cueIndex,
+              );
+            },
+          ),
+          // request focus to show keyboard when returning from song page
+        );
+      },
+      child: Row(
+        children: [
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                if (newBook) ...[
+                  Container(
+                    margin: const EdgeInsets.only(left: 15, top: 15),
+                    child: Text(
+                      '${book.displayName} énekeskönyv',
+                      style: TextStyle(
+                        color: Theme.of(context).colorScheme.primary,
+                        fontStyle: FontStyle.italic,
+                      ),
+                    ),
+                  ),
+                ],
+                if (newSong)
+                  Padding(
+                    padding:
+                        const EdgeInsets.only(left: 15, top: 10, bottom: 5),
+                    child: Text(
+                      '$songKey. ${songBooks[bookName][songKey]['title']}',
+                      style: Theme.of(context).textTheme.bodyLarge,
+                    ),
+                  ),
+                Container(
+                  decoration: BoxDecoration(
+                    border: Border(
+                      left: BorderSide(
+                        color: Theme.of(context).colorScheme.outline,
+                        width: 1,
+                      ),
+                    ),
+                  ),
+                  margin: const EdgeInsets.only(left: 20),
+                  padding: const EdgeInsets.all(7),
+                  child: RichText(
+                    text: TextSpan(
+                      // Without this explicit color, the search results would be
+                      // illegible when the app is in light mode. This makes it legible in
+                      // both dark and light modes.
+                      style: TextStyle(
+                        color: Theme.of(context).textTheme.bodyLarge!.color,
+                      ),
+                      children: [
+                        TextSpan(
+                          text: '$verseNumber. ',
+                          style: const TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        TextSpan(text: verseText)
+                      ],
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
-        ),
-        margin: const EdgeInsets.only(left: 10, right: 10),
-        padding: const EdgeInsets.all(7),
-        child: Row(
-          children: [
-            Expanded(
-              child: Text(
-                '${songBooks[book.name][songKey]['texts'][verseIndex].split('.')[0]}. versszak',
-              ),
-            ),
-            IconButton(
+          Padding(
+            padding: const EdgeInsets.only(right: 35),
+            child: IconButton(
               icon: const Icon(Icons.delete),
-              onPressed: () => settings.removeFromCueAt(settings.selectedCue, cueIndex),
+              onPressed: () =>
+                  settings.removeFromCueAt(settings.selectedCue, cueIndex),
             ),
-          ],
-        ),
-      ),
-    );
-  }
-
-  Widget songCard(Book book, String songKey, List<Widget> verseTiles) {
-    return Card(
-      clipBehavior: Clip.antiAlias,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Container(
-              margin: const EdgeInsets.only(
-                  left: 10, right: 10, top: 10, bottom: 5),
-              child: Text(
-                '$songKey. ${songBooks[book.name][songKey]['title']}',
-                style: Theme.of(context).textTheme.bodyLarge,
-              )),
-          ...verseTiles,
+          ),
         ],
       ),
     );
