4fef6ad630f9a63414917b99722fbb37834575ca
fodor.benedek2001 <fodor.benedek2001@gmail.com>
2025-01-31 22:52:16 +0000

    re-create android folder

--- android/app/src/main/kotlin/com/github/reformatus/enekeskonyv/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.github.reformatus.enekeskonyv
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- android/app/src/main/kotlin/com/github/reformatus/enekeskonyv_app/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.github.reformatus.enekeskonyv_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
