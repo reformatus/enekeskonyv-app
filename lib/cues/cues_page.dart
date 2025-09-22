7b33022c4232725ce7629c1458c5a3b2c276b841
Fodor Benedek <benedek@fodor.pro>
2025-09-22 09:47:53 +0200

    fix squashed buttons on cues page

--- lib/cues/cues_page.dart
+++ lib/cues/cues_page.dart
@@ -20,8 +20,8 @@ class CuesPage extends StatelessWidget {
     return Consumer<SettingsProvider>(
       builder: (context, settings, child) {
         return Scaffold(
-            appBar: AppBar(
-                title: DropdownButtonHideUnderline(
+          appBar: AppBar(
+            title: DropdownButtonHideUnderline(
               child: DropdownButton<String?>(
                 key: dropdownKey,
                 isExpanded: true,
@@ -37,16 +37,20 @@ class CuesPage extends StatelessWidget {
                   // Need to insert an element to the beginning of the list
                   // to account for New button
                   return ['', ...settings.cueStore.keys]
-                      .map((cue) => Center(
-                            widthFactor: 1,
-                            child: Text(
-                              settings.selectedCue,
-                              softWrap: false,
-                              overflow: TextOverflow.fade,
-                              style: const TextStyle(
-                                  fontSize: 20, fontWeight: FontWeight.normal),
+                      .map(
+                        (cue) => Center(
+                          widthFactor: 1,
+                          child: Text(
+                            settings.selectedCue,
+                            softWrap: false,
+                            overflow: TextOverflow.fade,
+                            style: const TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.normal,
                             ),
-                          ))
+                          ),
+                        ),
+                      )
                       .toList();
                 },
                 items: [
@@ -59,13 +63,16 @@ class CuesPage extends StatelessWidget {
                       children: [
                         Padding(
                           padding: const EdgeInsets.only(right: 8),
-                          child: Icon(Icons.add,
-                              color: Theme.of(context).colorScheme.primary),
+                          child: Icon(
+                            Icons.add,
+                            color: Theme.of(context).colorScheme.primary,
+                          ),
                         ),
                         Text(
                           'Új lista',
                           style: TextStyle(
-                              color: Theme.of(context).colorScheme.primary),
+                            color: Theme.of(context).colorScheme.primary,
+                          ),
                         ),
                       ],
                     ),
@@ -83,110 +90,116 @@ class CuesPage extends StatelessWidget {
                               softWrap: false,
                               overflow: TextOverflow.fade,
                               style: const TextStyle(
-                                  fontSize: 20, fontWeight: FontWeight.normal),
+                                fontSize: 20,
+                                fontWeight: FontWeight.normal,
+                              ),
                             ),
                           ),
                           IconButton(
                             icon: const Icon(Icons.delete),
                             onPressed: () =>
                                 showDeleteCueDialog(cue, context, settings),
-                          )
+                          ),
                         ],
                       ),
                     ),
                   ),
                 ],
               ),
-            )),
-            // Button to start displaying the cue from the beginning
-            floatingActionButton: settings.getSelectedCueContent().isEmpty
-                ? null
-                : FloatingActionButton(
-                    tooltip: 'Lejátszás az elejétől',
-                    onPressed: () {
-                      var verseId = settings.getSelectedCueContent()[0];
-                      List<String> parts = verseId.split('.');
-                      String songKey = parts[1];
-                      int verseIndex = int.parse(parts[2]);
-                      Book book =
-                          Book.values.firstWhere((b) => b.name == parts[0]);
+            ),
+          ),
+          // Button to start displaying the cue from the beginning
+          floatingActionButton: settings.getSelectedCueContent().isEmpty
+              ? null
+              : FloatingActionButton(
+                  tooltip: 'Lejátszás az elejétől',
+                  onPressed: () {
+                    var verseId = settings.getSelectedCueContent()[0];
+                    List<String> parts = verseId.split('.');
+                    String songKey = parts[1];
+                    int verseIndex = int.parse(parts[2]);
+                    Book book = Book.values.firstWhere(
+                      (b) => b.name == parts[0],
+                    );
 
-                      Navigator.of(context).push(
-                        MaterialPageRoute(
-                          builder: (context) {
-                            return SongPage(
-                              book: book,
-                              songIndex: songBooks[book.name]
-                                  .keys
-                                  .toList()
-                                  .indexOf(songKey),
-                              verseIndex: verseIndex,
-                              initialCueIndex: 0,
-                            );
-                          },
-                        ),
-                      );
-                    },
-                    child: const Icon(Icons.play_arrow),
-                  ),
-            body: Column(
-              children: [
-                Material(
-                  elevation: 5,
-                  child: SizedBox(
-                    height: 45,
-                    child: SafeArea(
-                      child: FadingEdgeScrollView.fromScrollView(
-                        child: ListView(
-                          controller: ScrollController(),
-                          scrollDirection: Axis.horizontal,
-                          padding: const EdgeInsets.all(6),
-                          children: [
-                            /* // TODO #50
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (context) {
+                          return SongPage(
+                            book: book,
+                            songIndex: songBooks[book.name].keys
+                                .toList()
+                                .indexOf(songKey),
+                            verseIndex: verseIndex,
+                            initialCueIndex: 0,
+                          );
+                        },
+                      ),
+                    );
+                  },
+                  child: const Icon(Icons.play_arrow),
+                ),
+          body: Column(
+            children: [
+              Material(
+                elevation: 5,
+                child: SizedBox(
+                  height: 45,
+                  child: SafeArea(
+                    bottom: false,
+                    top: false,
+                    child: FadingEdgeScrollView.fromScrollView(
+                      child: ListView(
+                        controller: ScrollController(),
+                        scrollDirection: Axis.horizontal,
+                        padding: const EdgeInsets.all(6),
+                        children: [
+                          /* // TODO #50
                             ElevatedButton.icon(
                               label: const Text('Lista beolvasás'),
                               onPressed: null,
                               icon: const Icon(Icons.qr_code_scanner),
                             ),*/
-                            const SizedBox(width: 5),
-                            ElevatedButton.icon(
-                              label: const Text('Megosztás'),
-                              onPressed:
-                                  settings.getSelectedCueContent().isEmpty
-                                      ? null
-                                      : () => showShareDialog(
-                                          context, settings.selectedCue,
-                                          cueContent:
-                                              settings.getSelectedCueContent()),
-                              icon: const Icon(Icons.share),
-                            ),
-                            const SizedBox(width: 5),
-                            ElevatedButton.icon(
-                              label: const Text('Ének hozzáfűzés'),
-                              onPressed: () => Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (context) => SearchPage(
-                                    book: settings.book,
-                                    settingsProvider: settings,
-                                    addToCueSearch: true,
+                          const SizedBox(width: 5),
+                          ElevatedButton.icon(
+                            label: const Text('Megosztás'),
+                            onPressed: settings.getSelectedCueContent().isEmpty
+                                ? null
+                                : () => showShareDialog(
+                                    context,
+                                    settings.selectedCue,
+                                    cueContent: settings
+                                        .getSelectedCueContent(),
                                   ),
+                            icon: const Icon(Icons.share),
+                          ),
+                          const SizedBox(width: 5),
+                          ElevatedButton.icon(
+                            label: const Text('Ének hozzáfűzés'),
+                            onPressed: () => Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => SearchPage(
+                                  book: settings.book,
+                                  settingsProvider: settings,
+                                  addToCueSearch: true,
                                 ),
                               ),
-                              icon: const Icon(Icons.manage_search),
                             ),
-                          ],
-                        ),
+                            icon: const Icon(Icons.manage_search),
+                          ),
+                        ],
                       ),
                     ),
                   ),
                 ),
-                Expanded(
-                  child: settings.getSelectedCueContent().isEmpty
-                      ? SafeArea(
-                          child: ListTile(
-                            subtitle: Text(
-                              '''
+              ),
+              Expanded(
+                child: settings.getSelectedCueContent().isEmpty
+                    ? SafeArea(
+                        child: ListTile(
+                          subtitle: Text(
+                            '''
 Itt jelennek meg a Kedvencként jelölt versszakok.
 ${settings.scoreDisplay == ScoreDisplay.all ? 'Kedvencnek jelöléshez használd a csillag gombot a kotta bal alsó sarkában.' : 'Kedvencnek jelöléshez nyomd hosszan a kívánt versszakot.'}
 
@@ -194,33 +207,40 @@ Tippek:
 - Több listát is készíthetsz a felső sávra koppintva. Mindig a fent kiválasztott listát szerkesztheted az ének oldalán.
 - A listáidat meg is oszthatod. Ha egy más által készített lista linkjét megnyitod, azt hozzáadjuk a listáidhoz.
 - Ha egy versszakot többször hozzá szeretnél adni, vagy csak gyorsan szeretnél haladni, használd az Ének hozzáfűzés gombot a felső sávban.''',
-                              style: TextStyle(
-                                color: Theme.of(context).colorScheme.secondary,
-                                fontStyle: FontStyle.italic,
-                              ),
+                            style: TextStyle(
+                              color: Theme.of(context).colorScheme.secondary,
+                              fontStyle: FontStyle.italic,
                             ),
                           ),
-                        )
-                      : SafeArea(
-                          child: ReorderableListView(
-                            onReorder: (oldIndex, newIndex) =>
-                                settings.reorderCue(
-                                    settings.selectedCue, oldIndex, newIndex),
-                            physics: Platform.isIOS
-                                ? const BouncingScrollPhysics()
-                                : null,
-                            children: getVerseTiles(settings),
-                          ),
                         ),
-                ),
-              ],
-            ));
+                      )
+                    : SafeArea(
+                        child: ReorderableListView(
+                          onReorder: (oldIndex, newIndex) =>
+                              settings.reorderCue(
+                                settings.selectedCue,
+                                oldIndex,
+                                newIndex,
+                              ),
+                          physics: Platform.isIOS
+                              ? const BouncingScrollPhysics()
+                              : null,
+                          children: getVerseTiles(settings),
+                        ),
+                      ),
+              ),
+            ],
+          ),
+        );
       },
     );
   }
 
   Future showDeleteCueDialog(
-      String cueName, BuildContext context, SettingsProvider settings) {
+    String cueName,
+    BuildContext context,
+    SettingsProvider settings,
+  ) {
     return showDialog(
       context: context,
       builder: (BuildContext context) => AlertDialog(
@@ -236,7 +256,8 @@ Tippek:
               settings.clearCue(cueName);
               try {
                 settings.changeSelectedCue(
-                    settings.cueStore.keys.firstWhere((cue) => cue != cueName));
+                  settings.cueStore.keys.firstWhere((cue) => cue != cueName),
+                );
               } catch (_) {
                 settings.changeSelectedCue(SettingsProvider.defaultSelectedCue);
               }
@@ -283,9 +304,7 @@ Tippek:
           title: const Text('Új lista'),
           content: TextField(
             controller: controller,
-            decoration: const InputDecoration(
-              labelText: 'Lista neve',
-            ),
+            decoration: const InputDecoration(labelText: 'Lista neve'),
             autofocus: true,
             onSubmitted: (text) => onTap(text),
             textInputAction: TextInputAction.done,
@@ -320,8 +339,15 @@ Tippek:
 
       verseTiles.add(
         // Had to factor out for tile removing closure to work properly (???)
-        verseTile(bookName, songKey, verseIndex, lastBook != bookName,
-            lastSong != songKey, i, settings),
+        verseTile(
+          bookName,
+          songKey,
+          verseIndex,
+          lastBook != bookName,
+          lastSong != songKey,
+          i,
+          settings,
+        ),
       );
       lastBook = bookName;
       lastSong = songKey;
@@ -332,8 +358,15 @@ Tippek:
     return verseTiles;
   }
 
-  Widget verseTile(String bookName, String songKey, int verseIndex,
-      bool newBook, bool newSong, int cueIndex, SettingsProvider settings) {
+  Widget verseTile(
+    String bookName,
+    String songKey,
+    int verseIndex,
+    bool newBook,
+    bool newSong,
+    int cueIndex,
+    SettingsProvider settings,
+  ) {
     Book book = Book.values.firstWhere((b) => b.name == bookName);
     String verse = songBooks[bookName][songKey]['texts'][verseIndex];
     String verseNumber = verse.split('.')[0];
@@ -375,8 +408,11 @@ Tippek:
                 ],
                 if (newSong)
                   Padding(
-                    padding:
-                        const EdgeInsets.only(left: 15, top: 15, bottom: 5),
+                    padding: const EdgeInsets.only(
+                      left: 15,
+                      top: 15,
+                      bottom: 5,
+                    ),
                     child: Text(
                       '$songKey. ${songBooks[bookName][songKey]['title']}',
                       style: Theme.of(context).textTheme.bodyLarge,
@@ -395,8 +431,10 @@ Tippek:
                   padding: const EdgeInsets.all(11),
                   child: Row(
                     children: [
-                      Text('$verseNumber. ',
-                          style: const TextStyle(fontWeight: FontWeight.bold)),
+                      Text(
+                        '$verseNumber. ',
+                        style: const TextStyle(fontWeight: FontWeight.bold),
+                      ),
                       Expanded(
                         child: Text(
                           verseText,
