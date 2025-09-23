8b47fea9db0cdc3243a14a420588fc55989bdb25
Fodor Benedek <benedek@fodor.pro>
2025-09-23 17:47:39 +0200

    add url

--- lib/news/news_service.dart
+++ lib/news/news_service.dart
@@ -3,13 +3,13 @@ import 'package:http/http.dart' as http;
 import 'news.dart';
 
 class NewsService {
-  static const String defaultNewsApiUrl = 'https://reformatus.github.io/enekeskonyv-app/news.json';
-  
+  static const String defaultNewsApiUrl = 'https://reflabs.hu/api/news.json';
+
   static Future<List<News>> fetchNews({String? customUrl}) async {
     try {
       final url = customUrl ?? defaultNewsApiUrl;
       final response = await http.get(Uri.parse(url));
-      
+
       if (response.statusCode == 200) {
         final List<dynamic> jsonList = json.decode(response.body);
         return jsonList.map((json) => News.fromJson(json)).toList();
